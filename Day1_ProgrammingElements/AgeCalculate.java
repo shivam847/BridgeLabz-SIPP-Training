8c732b2ce26a98dc363af3563dbc3e937d2f610d
Shivam Sisodiya <116007847+shivam847@users.noreply.github.com>
2025-07-31 13:38:43 +0530

    Create AgeCalculate.java

---
+++ Day1_ProgrammingElements/AgeCalculate.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+      int birthYear=2004;
+      int currYear=2024;
+      System.out.print("Harry's age in 2024 is ");
+      System.out.println(currYear-birthYear);
+      }
+}
