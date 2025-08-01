0f737cd19c45a9e53b7ef1e56b64b484d18feaae
shivam847 <shivam.sisodiya_cs22@gla.ac.in>
2025-08-01 21:42:47 +0530

    Add: Basic Calculator

---
+++ Day1_ProgrammingElements/Level2/Calculator.java
@@ -0,0 +1,29 @@
+package Level2;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        float number1 = scanner.nextFloat();
+
+        System.out.print("Enter second number: ");
+        float number2 = scanner.nextFloat();
+        float addition = number1 + number2;
+        float subtraction = number1 - number2;
+        float multiplication = number1 * number2;
+        float division = number2 != 0 ? number1 / number2 : Float.NaN;
+
+        
+        System.out.println("The addition, subtraction, multiplication, and division value of 2 numbers "
+                + number1 + " and " + number2 + " is "
+                + addition + ", " + subtraction + ", " + multiplication + ", and " + division);
+
+        
+        scanner.close();
+	}
+
+}
