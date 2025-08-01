da56ddb91e7b846ee5f3393fbc9d0afdad8bd130
shivam847 <shivam.sisodiya_cs22@gla.ac.in>
2025-08-01 21:30:39 +0530

    initial commit

---
+++ Day1_ProgrammingElements/level1/AgeCalculater.java
@@ -0,0 +1,16 @@
+package level1;
+
+public class AgeCalculater {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int birthYear = 2000;
+        int currentYear = 2024;
+
+        int age = currentYear - birthYear;
+
+        System.out.println("Harry's age in 2024 is " + age);
+    }
+	}
+
+
